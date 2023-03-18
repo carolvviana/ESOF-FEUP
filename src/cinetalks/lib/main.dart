1ea1d87cd38711fcfc4d29aab3f1d1685ba40ff7
Sérgio Peixoto <sergiopeixoto3@gmail.com>
2023-03-18 02:14:53 +0000

    login and sign up page done with firebase api integration, also some responsivity issues fixed

--- src/cinetalks/lib/main.dart
+++ src/cinetalks/lib/main.dart
@@ -1,24 +1,63 @@
+import 'package:cinetalks/widgets/login_widget.dart';
+import 'package:cinetalks/widgets/signup_widget.dart';
 import 'package:flutter/material.dart';
+
 import 'screens/home_screen.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+Future main() async {
   Paint.enableDithering = true;
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       title: "CineTalks",
-      home: HomeScreen(),
+      home: MainPage(),
     );
   }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text("Error"),
+              );
+            } else if (snapshot.hasData) {
+              return const HomeScreen();
+            } else {
+              return const LoginWidget();
+            }
+          },
+        ),
+        backgroundColor: const Color(0xff2a2a2a),
+      );
 }
